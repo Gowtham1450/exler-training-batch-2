3f31377d3c8d9a3b2458bb84b9540cd6615a8ca9
Gowtham1450 <gowtham.yella08@gmail.com>
2025-01-20 09:58:35 +0530

    demo029

---
+++ demo029.java
@@ -0,0 +1,20 @@
+public class Demo029 {
+    public static void main(String[] args) {
+                String[] names = {
+            "Rajesh",
+            "Srinith",
+            "Pavan",
+            "Adithya",
+            "Sunny",
+            "Homakanth",
+            "Harish",
+            "Revan",
+            "Ian",
+            "Julia"
+        };
+        System.out.println("The names in the array are:");
+        for (String name : names) {
+            System.out.println(name);
+        }
+    }
+}
